86c654f5108b30b89d88fd3441c6150f9724e06c
Jack Jenkins <jackjenkins97@gmail.com>
2019-04-20 18:00:29 +0100

    Logo changed from svg to png

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../logo.svg';
+import logo from '../logo.png';
 import basket from '../shopping-cart.svg';
 import styled from 'styled-components';
 import {ButtonContainer} from './Button';
